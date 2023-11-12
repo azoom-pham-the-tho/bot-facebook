c3f9d187bf80f6feb72767f04a653b607e73cbdd
pham.the.tho <pham.the.tho@azoom.jp>
2023-11-12 15:22:45 +0700

    Add send file

---
+++ data.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    question: "hello",
+    answer:
+      "hi, tôi có thể giúp gì cho bạn ?",
+  },
+  {
+    question: "cho xin tấm ảnh",
+    answer: "Ảnh thời thành công ... Nhân",
+    fileName:"image.jpg"
+  },
+  {
+    question: "cho xin video",
+    answer: "Video về game",
+    fileName:"video.mp4"
+  },
+  {
+    question: "cho xin JD nodejs",
+    answer: "Link JD nodejs",
+    fileName:"JDnodejs.pptx",
+    link:"https://docs.google.com/presentation/d/1nSZWgiKtwZGgnwhIxOcODCO20_jBG3xP/edit?usp=sharing&ouid=116696043866188693065&rtpof=true&sd=true"
+  },
+  {
+    question: "bạn ăn cơm chưa",
+    answer:
+      "cơm thì ai mà chả ăn, bạn sợ tôi ăn mất phần của bạn à :)",
+  },
+];
